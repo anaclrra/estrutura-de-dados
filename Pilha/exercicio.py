5451506163a57b59f68766a1a00bade9dc615292
Ana Clara <clara.bonifacio@escolar.ifrn.edu.br>
2023-04-25 11:34:31 -0300

    Criando estrutura pilha

---
+++ Pilha/exercicio.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+class Pilha:
+    def __init__(self):
+        self.topo = None
+        self.tamanho = 0
+
+    def is_empty(self):
+        pass
+
+    def push(self):
+        ...
+    def pop(self):
+        ...
+    def preek(self):
+        ...
+    def list_items(self):
+        ...
+    def get_size(self):
+        ...
